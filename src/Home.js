1421fd82f11e1f207ecf37081d005fafa62dfc2f
Jaide Vlahovic <t.vlahovic@protonmail.ch>
2020-08-12 05:11:38 -0600

    Second commit

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,26 @@
 import React from "react";
+import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
-    <div>
-      <h1>I am the homepage</h1>
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://images-na.ssl-images-amazon.com/images/G/15/kindle/journeys/MzgyMjg2ZDEt/MzgyMjg2ZDEt-OTc0YjlhNjct-w1500._CB408087835_.jpg"
+        alt=""
+      />
+      <Product
+        id="1230123"
+        title="Wine Enthusiast Private Preserve Wine Preservation Spray"
+        price={34.69}
+        rating={5}
+        image="https://images-na.ssl-images-amazon.com/images/I/81DuVo508jL._AC_SX522_.jpg"
+      />
     </div>
   );
 }
 
 export default Home;
+
+// Product id, title, price, rating, image
